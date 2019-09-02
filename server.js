c61d33cf026fc3abc2fcd929448b4eb28753ace8
arodriguuij <arodriguuij@gmail.com>
2019-09-02 12:00:17 +0200

    Create MongoBD Atlas and connect it with the app

--- server.js
+++ server.js
@@ -1,9 +1,48 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config( {path: './config.env'} ); //Read config.env  
 
 const app = require('./app');
-
 //console.log(process.env);
+
+
+
+// TODO: Connect with de the DB and create Schemas and models
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB connection successful!'));
+
+const tourSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        require: [true, 'A tour must have a price']
+    }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: "The Park",
+    price: 497
+});
+
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log('Error '+err);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
