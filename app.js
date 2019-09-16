8766eb53efd7fd9120e2bef46f635cfbe866462c
arodriguuij <arodriguuij@gmail.com>
2019-09-16 11:04:06 +0200

    Sending JWT via Cookie. Implementing tate limiting. Setting Security HTTP headeres with Helmet. Implementing data sanitization with mongoSanitize and xss. Preenting parameter pollution with hpp

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,18 +13,57 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// TODO: 1) MIDDLEWARES
+// TODO: 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+
+// Development login
 //console.log(`App: ${process.env.PORT}`);
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
-app.use(express.json()); // Middleware -> Parse data from the body
+
+// Limit request from same API
+// How many request per IP we are going to allow in a certain amount of time
+const limiter = rateLimit({
+    max: 100,  //numbers of request from the same IP
+    windowMS: 60 * 60 * 1000,  // in 1h -> 60minutes * 60seconds * 1000milliseconds
+    message: 'Too many request from this IP, please try again in an hour.'
+});
+app.use('/api', limiter); // Afect all of the routs that start with this '/api'
+
+
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb'})); // Middleware -> Parse data from the body ... Limit 10kb from the req.body
+
+// Data sanitization against NoSQL query injection
+// From example: Login -> {"email": {"$gt": ""}, "password": "pass1234"} we had access
+// Look the request body, request query string, and also at request params, and filter out all of the dollar sign s and dots.
+app.use(mongoSanitize()); // Retun middleware function
+
+
+// Data sanitization against XSS atack
+app.use(xss()); //Clean html symbols injection
+// Example:	"name": "<div id='bad-code>Name</div>" --->  "name": "&lt;div id='bad-code>Name&lt;/div>",
+
+
+// Prevent parameter pollution
+app.use(hpp({
+    // whitelist -> Array for which we actually allow duplicates in the query string.
+    whitelist: [ 'duration', 'ratingAverage', 'ratingCuantity', 'maxGroupSize', 'difficulty', 'price']
+}));
+// Example: {{URL}}api/v1/tours?sort=duration&sort=price  -> Sort by 2 different types -> Solution: useing the lastone
+
 
 // Open to the browser from the folder and no from the route 
-// app.use(express.static(`${__dirname}/public`)); 
+// Serving stativ files
+app.use(express.static(`${__dirname}/public`));
 // http://localhost:3000/overview.html
 
+
+// TEST middleware
 // Declaration of our own middleware (next Function to execute)
 app.use((req, res, next) => {
     //console.log('Hello from the middleware¡');
@@ -29,9 +73,11 @@ app.use((req, res, next) => {
     next();
 });
 
+
 // TODO: 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
 
 // Defaults routes
 app.all('*', (req, res, next) => {
@@ -51,8 +97,10 @@ app.all('*', (req, res, next) => {
    next(new AppError(`Cannot find  ${req.originalUrl} on this server!`, 404));
 });
 
+
 // 4 arguments, express is gonna reconize automaticaly as a erron handler middleware
 app.use(globalErrorHandler);
+
 
 // TODO: 3) EXPORTS
 module.exports = app;
--- controllers/autenticationController.js
+++ controllers/autenticationController.js
@@ -14,6 +14,24 @@ const signToken = id => {
 
 const createSentToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+
+    const cookieOptions = {
+        // * 24 * 60 * 60 * 1000 converting to milisecond 
+        // secure: true -> Cookie only be send on an encypted connection (HTTPS)
+        // httpOnly: true -> Cookie cannot be accessedor modified in any way by the browser 
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+        cookieOptions.secure = true;
+    }
+
+    res.cookie('jwt', token, cookieOptions);
+
+    // Remove the password from the output
+    user.password = undefined;
+    
     res.status(statusCode).json({
         status: 'success',
         token,
